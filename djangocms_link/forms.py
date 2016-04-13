1e1f811b64bc73a7bc0c9e2d61511bd04fe67185
Daniel Mahlberg <dm@getaweb.at>
2016-04-13 16:02:09 +0200

    cleaned up imports

--- djangocms_link/forms.py
+++ djangocms_link/forms.py
@@ -1,29 +1,9 @@
 from django.forms.models import ModelForm
-from django.utils.translation import ugettext_lazy as _
-from django_select2.widgets import AutoHeavySelect2Widget
 from djangocms_link.models import Link
-from cms.models import Page
 from django.forms.widgets import Media
 
 
 class LinkForm(ModelForm):
-    #try:
-    #    from djangocms_link.fields import PageSearchField
-    #    page_link = PageSearchField(required=False,
-    #                                queryset=Page.objects.drafts(),
-    #                                widget=AutoHeavySelect2Widget(
-    #                                    select2_options={
-    #                                        'placeholder': _("page"),
-    #                                        'required': False,
-    #                                        'initial': None,
-    #                                        'help_text': u"A link to a page has priority over a text link.",
-    #                                    }
-    #                                ),
-    #                                label=_("page")
-    #                                )
-    #except ImportError:
-    #    from cms.forms.fields import PageSelectFormField
-    #    page_link = PageSelectFormField(queryset=Page.objects.drafts(), label=_("page"), required=False)
 
     def for_site(self, site):
         # override the page_link fields queryset to containt just pages for
